8392925e1a40b953d598c4183d832901617c2364
0qinghao <shu_rin@shu.edu.cn>
2020-07-04 18:58:37 +0800

    github+.nojekyll+默认路径编译；coding+docs路径编译+only push docs

--- push_2_github_and_coding.sh
+++ push_2_github_and_coding.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 bundle exec jekyll build --destination ./docs
 
-<<<<<<< HEAD
 # touch docs/baidu_sitemap.xml
-=======
-touch docs/baidu_sitemap.xml
->>>>>>> 1976014956ebdc4801b2ea70e5aefbdadb836cee
 # sed -e 's/0qinghao.github.io\/inforest/7u7d9c.coding-pages.com/' docs/sitemap.xml> docs/baidu_sitemap.xml
 
 git add -A;git commit -m "$*";git push;
