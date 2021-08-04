0cf3f94f8a4bcc95d3246665f70256aee03c4c3d
qhlin <qhlin96@foxmail.com>
2021-08-04 15:50:15 +0800

    重新配置

---
+++ push_expect.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/expect
+set githubpw [lindex $argv 0]
+# set codingpw [lindex $argv 1]
+
+cd ../0qinghao.github.io
+# spawn git push coding master
+# expect {
+#     "Password*" {send "$codingpw\n"}
+# }
+# expect eof
+
+spawn git push githubpage master
+expect {
+    "Password*" {send "$githubpw\n"}
+}
+expect eof
+
+cd ../jekyll.0qinghao.github.io
+spawn git push origin master
+expect {
+    "Password*" {send "$githubpw\n"}
+}
+expect eof
